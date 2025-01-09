7d539d7fa51ea07c8b77d0cf84b9ae32e06d4d88
sigleto <sigleto@gmail.com>
2025-01-09 22:36:22 +0100

    titulo

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import PrincipalStack from './Navigation';
 
-
 const App: React.FC = () => {
+  useEffect(() => {
+    document.title = "Simuladores Financieros";
+  }, []);
+
   return (
     <Router>
       <div className="app-container">
-       
         <main className="main-content">
           <PrincipalStack />
         </main>
